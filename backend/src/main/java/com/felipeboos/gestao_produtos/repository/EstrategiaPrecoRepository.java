448b3954c2a1142bf9c2fcb408b14a41be5d371c
FelipeBoos <felipeboos18@gmail.com>
2026-03-12 19:42:41 -0300

    feat(preco-entity): create EstrategiaPreco repository

---
+++ backend/src/main/java/com/felipeboos/gestao_produtos/repository/EstrategiaPrecoRepository.java
@@ -0,0 +1,8 @@
+package com.felipeboos.gestao_produtos.repository;
+
+import com.felipeboos.gestao_produtos.entity.EstrategiaPreco;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstrategiaPrecoRepository extends JpaRepository<EstrategiaPreco, Long> {
+
+}
